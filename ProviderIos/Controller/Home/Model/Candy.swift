4205eafb501fc274782a23c51be2c6426a7f317b
Thang <quangthang.le@tanchonggroup.com>
2018-11-06 17:16:56 +0700

    cm

---
+++ ProviderIos/Controller/Home/Model/Candy.swift
@@ -0,0 +1,19 @@
+//
+//  Candy.swift
+//  ProviderIos
+//
+//  Created by Fullname on 11/6/18.
+//  Copyright © 2018 Fullname. All rights reserved.
+//
+
+import UIKit
+
+class Candy {
+    
+    var category: String
+    var name: String
+    init(category:String,name: String) {
+        self.name = name
+        self.category = category
+    }
+}
--- ProviderIos/Controller/Home/View/Controller/HomeViewController.swift
+++ ProviderIos/Controller/Home/View/Controller/HomeViewController.swift
@@ -13,12 +13,42 @@ class HomeViewController: BaseViewController {
     
     var appServerClient =  AppServerClient()
 
-    
+    var searchController = UISearchController(searchResultsController: nil)
+    var candies = [
+    Candy(category:"Chocolate", name:"Chocolate Bar"),
+    Candy(category:"Chocolate", name:"Chocolate Chip"),
+    Candy(category:"Chocolate", name:"Dark Chocolate"),
+    Candy(category:"Hard", name:"Lollipop"),
+    Candy(category:"Hard", name:"Candy Cane"),
+    Candy(category:"Hard", name:"Jaw Breaker"),
+    Candy(category:"Other", name:"Caramel"),
+    Candy(category:"Other", name:"Sour Chew"),
+    Candy(category:"Other", name:"Gummi Bear"),
+    Candy(category:"Other", name:"Candy Floss"),
+    Candy(category:"Chocolate", name:"Chocolate Coin"),
+    Candy(category:"Chocolate", name:"Chocolate Egg"),
+    Candy(category:"Other", name:"Jelly Beans"),
+    Candy(category:"Other", name:"Liquorice"),
+    Candy(category:"Hard", name:"Toffee Apple")
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
          getFriends()
+        searchController.searchResultsUpdater = self
+        if #available(iOS 9.1, *) {
+            searchController.obscuresBackgroundDuringPresentation = false
+        } else {
+            // Fallback on earlier versions
+        }
+        searchController.searchBar.placeholder = "Search Candies"
+        if #available(iOS 11.0, *) {
+            navigationItem.searchController = searchController
+        } else {
+            // Fallback on earlier versions
+        }
+        definesPresentationContext = true
       
     }
     
@@ -38,4 +68,11 @@ class HomeViewController: BaseViewController {
     }
     
     
+}
+
+extension HomeViewController: UISearchResultsUpdating {
+    // MARK: - UISearchResultsUpdating Delegate
+    func updateSearchResults(for searchController: UISearchController) {
+        // TODO
+    }
 }
