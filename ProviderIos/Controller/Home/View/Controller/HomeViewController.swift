a5956b636edc355b75c0ebafb565bba2408c2bdd
Thang <quangthang.le@tanchonggroup.com>
2018-11-14 15:45:32 +0700

    cm

--- ProviderIos/Controller/Home/View/Controller/HomeViewController.swift
+++ ProviderIos/Controller/Home/View/Controller/HomeViewController.swift
@@ -8,71 +8,83 @@
 
 import UIKit
 import Alamofire
-class HomeViewController: BaseViewController {
+class HomeViewController: BaseViewController, UITableViewDataSource, UITableViewDelegate {
 
     
-    var appServerClient =  AppServerClient()
 
-    var searchController = UISearchController(searchResultsController: nil)
-    var candies = [
-    Candy(category:"Chocolate", name:"Chocolate Bar"),
-    Candy(category:"Chocolate", name:"Chocolate Chip"),
-    Candy(category:"Chocolate", name:"Dark Chocolate"),
-    Candy(category:"Hard", name:"Lollipop"),
-    Candy(category:"Hard", name:"Candy Cane"),
-    Candy(category:"Hard", name:"Jaw Breaker"),
-    Candy(category:"Other", name:"Caramel"),
-    Candy(category:"Other", name:"Sour Chew"),
-    Candy(category:"Other", name:"Gummi Bear"),
-    Candy(category:"Other", name:"Candy Floss"),
-    Candy(category:"Chocolate", name:"Chocolate Coin"),
-    Candy(category:"Chocolate", name:"Chocolate Egg"),
-    Candy(category:"Other", name:"Jelly Beans"),
-    Candy(category:"Other", name:"Liquorice"),
-    Candy(category:"Hard", name:"Toffee Apple")
-    ]
+    @IBOutlet weak var tableView: UITableView!
+
+    @IBOutlet weak var searchFooter: SearchFooter!
+    var detailViewController: DetailViewController? = nil
+    var candies = [Candy]()
+    var filteredCandies = [Candy]()
+    let searchController = UISearchController(searchResultsController: nil)
     
+    // MARK: - View Setup
     override func viewDidLoad() {
         super.viewDidLoad()
+ 
         
-         getFriends()
-        searchController.searchResultsUpdater = self
-        if #available(iOS 9.1, *) {
-            searchController.obscuresBackgroundDuringPresentation = false
-        } else {
-            // Fallback on earlier versions
-        }
-        searchController.searchBar.placeholder = "Search Candies"
-        if #available(iOS 11.0, *) {
-            navigationItem.searchController = searchController
-        } else {
-            // Fallback on earlier versions
-        }
-        definesPresentationContext = true
-      
+        candies = [
+            Candy(category:"Chocolate", name:"Chocolate Bar"),
+            Candy(category:"Chocolate", name:"Chocolate Chip"),
+            Candy(category:"Chocolate", name:"Dark Chocolate"),
+            Candy(category:"Hard", name:"Lollipop"),
+            Candy(category:"Hard", name:"Candy Cane"),
+            Candy(category:"Hard", name:"Jaw Breaker"),
+            Candy(category:"Other", name:"Caramel"),
+            Candy(category:"Other", name:"Sour Chew"),
+            Candy(category:"Other", name:"Gummi Bear"),
+            Candy(category:"Other", name:"Candy Floss"),
+            Candy(category:"Chocolate", name:"Chocolate Coin"),
+            Candy(category:"Chocolate", name:"Chocolate Egg"),
+            Candy(category:"Other", name:"Jelly Beans"),
+            Candy(category:"Other", name:"Liquorice"),
+            Candy(category:"Hard", name:"Toffee Apple")]
+        
+    
     }
     
-    func getFriends() {
-        showDialog()
-        appServerClient.getFriends(completion: { [weak self] result in
-            self?.dismissDialog()
-            switch result {
-            case .success(let friends):
-                print(friends)
-            case .failure( _):
-                print("Can not get data")
-                break
-                
+    override func viewWillAppear(_ animated: Bool) {
+     
+        super.viewWillAppear(animated)
+    }
+    
+    // MARK: - Table View
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+     
+        return candies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+     
+        let candy = candies[indexPath.row]
+    
+        cell.textLabel!.text = candy.name
+        cell.detailTextLabel!.text = candy.category
+        return cell
+    }
+    
+    // MARK: - Segues
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showDetail" {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                let  candy = candies[indexPath.row]
+            
+                let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
+                controller.detailCandy = candy
+                controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
+                controller.navigationItem.leftItemsSupplementBackButton = true
             }
-        })
+        }
     }
     
-    
+    // MARK: - Private instance methods
+
 }
 
-extension HomeViewController: UISearchResultsUpdating {
-    // MARK: - UISearchResultsUpdating Delegate
-    func updateSearchResults(for searchController: UISearchController) {
-        // TODO
-    }
-}
